f33234f490071d6f5e732b0bd0557c9417ef5fed
ErickCardoso007 <cyber666king@gmail.com>
2024-07-25 19:23:18 -0300

    create header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
+import { Header } from './components/Header'
 import './App.css'
+
 function App() {
 
 
   return (
-    <>
-      <div className="header">
-        <div className="container-text">
-          <span>GANGSTER zz</span>
-        </div>
-      <nav className='container-links'>
-        <a>HOME</a>
-        <a>CLIENTS</a>
-        <a>BLOG</a>
-        <a>CONTACT</a>
-      </nav>
-      </div>
-    </>
+      <>
+      <Header />
+     </>
   )
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+export const Header = () => {
+    return (
+        <>
+            <div className="header">
+                <div className="container-text">
+                    <span>GANGSTER zz</span>
+                </div>
+                <nav className='container-links'>
+                    <a>HOME</a>
+                    <a>CLIENTS</a>
+                    <a>BLOG</a>
+                    <a>CONTACT</a>
+                </nav>
+            </div>
+        </>
+
+    )
+}
